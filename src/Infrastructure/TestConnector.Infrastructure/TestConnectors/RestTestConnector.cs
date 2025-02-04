c6be6fa25eb79e02a9b8f786d2add673c45351b8
vlados1kin <lashkin2004@gmail.com>
2025-02-04 19:08:10 +0300

    Implement get new trades method in the rest test connector

---
+++ src/Infrastructure/TestConnector.Infrastructure/TestConnectors/RestTestConnector.cs
@@ -0,0 +1,61 @@
+﻿using System.Globalization;
+using System.Text.Json;
+using TestConnector.Domain.Entities;
+using TestConnector.Domain.Interfaces;
+
+namespace TestConnector.Infrastructure.TestConnectors;
+
+public class RestTestConnector : IRestTestConnector
+{
+    private readonly HttpClient _httpClient; 
+    
+    public RestTestConnector(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+        _httpClient.BaseAddress = new Uri("https://api-pub.bitfinex.com/v2/");
+    }
+    
+    public async Task<IEnumerable<Trade>> GetNewTradesAsync(string pair, int? maxCount, int? sort, DateTimeOffset? start, DateTimeOffset? end)
+    {
+        var queryParameters = new Dictionary<string, string?>()
+        {
+            { "limit", maxCount?.ToString() },
+            { "sort", sort?.ToString() },
+            { "start", start?.ToUnixTimeMilliseconds().ToString() },
+            { "end", end?.ToUnixTimeMilliseconds().ToString() }
+        };
+
+        var queryBuilder = queryParameters.Where(kvp => kvp.Value != null).ToList();
+
+        var query = string.Join("&", queryBuilder.Select(kvp => $"{kvp.Key}={kvp.Value}"));
+        
+        var request = $"trades/{pair}/hist?{query}";
+        
+        var responseMessage = await _httpClient.GetAsync(request);
+        
+        responseMessage.EnsureSuccessStatusCode();
+
+        var json = await responseMessage.Content.ReadAsStringAsync();
+
+        var rawTrades = JsonSerializer.Deserialize<List<List<decimal>>>(json);
+        if (rawTrades == null)
+        {
+            return new List<Trade>();
+        }
+
+        return rawTrades.Select(rawTrade => new Trade
+            {
+                Pair = pair,
+                Price = rawTrade[3],
+                Amount = rawTrade[2],
+                Side = rawTrade[2] > 0 ? "buy" : "sell",
+                Time = DateTimeOffset.FromUnixTimeMilliseconds((long)rawTrade[1]),
+                Id = rawTrade[0].ToString(CultureInfo.InvariantCulture)
+            }).ToList();
+    }
+
+    public Task<IEnumerable<Candle>> GetCandleSeriesAsync(string pair, int periodInSec, DateTimeOffset? from, DateTimeOffset? to = null, long? count = 0)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Presentation/TestConnector.API/Controllers/RestTestController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestConnector.Domain.Interfaces;
+
+namespace TestConnector.API.Controllers;
+
+[ApiController]
+[Route("api/v1/trades")]
+public class RestTestController : ControllerBase
+{
+    private readonly IRestTestConnector _connector;
+    
+    public RestTestController(IRestTestConnector connector)
+    {
+        _connector = connector;
+    }
+
+    [HttpGet("{pair}")]
+    public async Task<IActionResult> GetNewTrades(
+        [FromRoute] string pair,
+        [FromQuery] int? maxCount,
+        [FromQuery] int? sort,
+        [FromQuery] DateTimeOffset? start,
+        [FromQuery] DateTimeOffset? end)
+    {
+        var trades = await _connector.GetNewTradesAsync(pair, maxCount, sort, start, end);
+
+        return Ok(trades);
+    }
+}
